8233ed09a0cc022ea64e963d14ee810934c35070
peaky76 <robertjamespeacock@gmail.com>
2026-05-22 22:19:13 +0100

    fix: change headers

--- src/extractors/bha_extractor.py
+++ src/extractors/bha_extractor.py
@@ -26,7 +26,9 @@ LAST_UPDATE_STR = str(get_last_occurrence_of(UPDATE_DAY)).replace("-", "")
 BHA_PAGE_URL = "https://www.britishhorseracing.com/regulation/official-ratings/ratings-database/"
 
 _HEADERS = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36"
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
+    "Accept-Language": "en-GB,en;q=0.9",
 }
 
 
