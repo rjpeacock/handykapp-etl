3b0832e3009852d792fde502c7789eb2beb1f17d
peaky76 <robertjamespeacock@gmail.com>
2026-05-26 15:59:49 +0100

    refactor: replace missing racecard workaround with rapid extractor in source_data_runner

--- src/runners/source_data_runner.py
+++ src/runners/source_data_runner.py
@@ -4,10 +4,10 @@ from pathlib import Path
 
 sys.path.append(str(Path(__file__).resolve().parent.parent))
 
-from prefect import flow, get_run_logger
+from prefect import flow
 
 from extractors.rapid_horseracing_extractor import (
-    replace_missing_theracingapi_racecards,  # type: ignore  # TEMP: replaces rapid_horseracing_extractor
+    rapid_horseracing_extractor,  # type: ignore
 )
 from extractors.theracingapi_extractor import (
     theracingapi_racecards_extractor,  # type: ignore
@@ -15,16 +15,9 @@ from extractors.theracingapi_extractor import (
 
 
 @flow
-def load_nothing_and_log():
-    logger = get_run_logger()
-    logger.info("I've run, but done nothing.")
-
-
-@flow
 def source_data_runner():
-    load_nothing_and_log()  # type: ignore
     theracingapi_racecards_extractor()  # type: ignore
-    replace_missing_theracingapi_racecards()  # type: ignore
+    rapid_horseracing_extractor()  # type: ignore
 
 
 if __name__ == "__main__":
